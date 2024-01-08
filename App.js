d9ff59111613251a4560fb8bb60de8aeb7dbba64
Anjali Dhobale <AnjaliD@centiro.se>
2024-01-08 20:53:58 +0530

    created functional component using jsx

--- App.js
+++ App.js
@@ -1,38 +1,33 @@
-/**
- * 
- * <div class="parent">
- *      <div class="child">
- *          <h1></h1>
- *      </div>
- *      <div class="child2">
- *          <h1></h1>
- *      </div>
- * </div>
- */        
-        
-        const parent = React.createElement('div', {
-            id:'parent'
-        }, [React.createElement('div', {
-            id:'child'
-        },React.createElement('h1', {
-   
-        }, "I am in h1") ), React.createElement('div', {
-            id:'child2'
-        },React.createElement('h1', {
-   
-        }, "I am in h1") )]);
-        
-        
-        // this empty object is a place where u can give attributes to your tags
-        // const heading = React.createElement('h1', {
-        //     id:'heading',
-        //     xyz:'xyz'
-        // }, "Hello world from React");
-        // console.log(heading)
-        //if u console this it will not show any h1 tag it gives an object element because it is react object
-        // 2 and 3 part is props (2-attribute 3 children which will go inside the h1 tag)
-        //the render method actually convert react object into h1 tag
-        //third argument is the children/content that u pass in, and it is an array so use it for siblings
-        const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-        root.render(parent);
+//using core react 
+    const element = React.createElement('div', {className:"title"}, [
+        React.createElement('h1', '', 'This is h1 tag'),
+        React.createElement('h2', '', 'This is h2 tag'),
+        React.createElement('h3', '', 'This is h3 tag')
+    ]);
+
+//using jsx
+    const jsxElement = <h1>JSX Element</h1>;
+    const JsxSmallComponent = () => { return (
+        <div>
+            <h1>Small</h1>
+        </div>
+      )
+    }
+    const JsxComponent = () =>  { 
+        return (
+        <div className='title'>
+            <h1>This is h1 tag</h1>
+            <h2>This is h2 tag</h2>
+            <h3>This is h3 tag</h3>
+            <JsxSmallComponent/>
+            {jsxElement}
+        </div>
+        )
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    root.render(<JsxComponent/>);
